1145659bab5962a14d2f480d8ee7ac24de5e1583
dora <dora.korpar@gmail.com>
2017-07-04 12:33:13 -0700

    No newline at eof

--- 0x02-getline/_getline.c
+++ 0x02-getline/_getline.c
@@ -1,6 +1,6 @@
 #include <unistd.h>
 #include <stdlib.h>
-
+#include <stdio.h>
 #include "_getline.h"
 
 /**
@@ -13,16 +13,30 @@
 char *_getline(const int fd)
 {
 	char *buf;
+	char *line;
+	int i, j;
 	size_t nread;
 
 	if ((buf = malloc(READ_SIZE)) == NULL)
 	{
 		return (NULL);
 	}
+	if ((line = malloc(READ_SIZE * 2)) == NULL)
+	{
+		return (NULL);							\
+	}
 
 	while ((nread = read(fd, buf, READ_SIZE)) > 0)
 	{
-		return (buf);
+		i = 0;
+		j = 0;
+		while (buf[i + 1]) /* i + 1 so last newline char is cut off from line */
+		{
+			line[j] = buf[i];
+			i++;
+			j++;
+		}
+		return (line);
 	}
 	return (NULL);
 }
