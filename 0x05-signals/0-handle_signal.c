f3e9b94bff45efa8a3040ad85f446d3fa495eebc
dora <dora.korpar@gmail.com>
2017-08-10 23:42:31 -0700

    Add readme and handle signal

--- 0x05-signals/0-handle_signal.c
+++ 0x05-signals/0-handle_signal.c
@@ -3,13 +3,19 @@
 
 #include "signals.h"
 
-sighandler_t handler(int signum)
+void handler(int signum)
 {
 	printf("Gotcha! [%d]\n", signum);
 }
 
+/**
+ * handle_signal - sets a handler for the signal SIGINT
+ *
+ * Return: EXIT_SUCCESS on success, -1 on failure
+ */
 int handle_signal(void)
 {
-	signal(SIGINT, handler(SIGINT));
+	if (signal(SIGINT, handler) == SIG_ERR)
+		return (-1);
 	return (EXIT_SUCCESS);
 }
---
+++ 0x05-signals/README.md
@@ -0,0 +1,5 @@
+0x05. Signals
+
+0-handle_signal.c - Functions that set signal handler to SIGINT
+
+signals.h - Header file containing prototypes for functions and variables
